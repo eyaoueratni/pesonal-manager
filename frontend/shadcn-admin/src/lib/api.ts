34f3d325a9f4d5a319c8efbe7eac4a244940da79
ouertani Aya <ouertani Aya>
2026-05-30 22:48:27 +0200

    changesss

--- frontend/shadcn-admin/src/lib/api.ts
+++ frontend/shadcn-admin/src/lib/api.ts
@@ -1,7 +1,7 @@
 import { useAuthStore } from '@/stores/auth-store'
 import axios, { type AxiosError } from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000'
+const API_URL = 'http://178.105.159.62:30800'
 
 export const api = axios.create({
   baseURL: API_URL,
